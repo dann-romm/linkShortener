a25d1e2b98c68021fad16c22088427c9fe6f303f
dann-romm <danilka.2002.lol@gmail.com>
2022-06-08 13:19:18 +0300

    503 returned when server is going to stop (application in a appStateHalt state)

--- cmd/linkShortener/main.go
+++ cmd/linkShortener/main.go
@@ -17,6 +17,7 @@ type Server struct {
 	StorageService storage.StorageService
 }
 
+// GetLink Handler for GET /link?short_link=<short_link>
 func (s *Server) GetLink(c echo.Context) error {
 	shortLink := c.QueryParam("short_link")
 	if shortLink == "" {
@@ -31,26 +32,55 @@ func (s *Server) GetLink(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{"full_link": fullLink})
 }
 
+// SaveLink Handler for POST /link?full_link=<full_link>
 func (s *Server) SaveLink(c echo.Context) error {
 	fullLink := c.QueryParam("full_link")
 	if fullLink == "" {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "full_link is required"})
 	}
-	shortLink, err := s.StorageService.SaveNewLink(context.Background(), fullLink)
+	shortLink, err := s.StorageService.SaveNewLink(context.Background(), transformLink(fullLink))
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
 	}
 	return c.JSON(http.StatusOK, map[string]string{"short_link": shortLink})
 }
 
+// function that truncates protocol, www and trailing slashes
+func transformLink(link string) string {
+	if len(link) >= 7 && link[0:7] == "http://" {
+		link = link[7:]
+	} else if len(link) >= 8 && link[0:8] == "https://" {
+		link = link[8:]
+	}
+	if len(link) >= 4 && link[0:4] == "www." {
+		link = link[4:]
+	}
+	lastSlash := len(link)
+	for lastSlash > 0 && link[lastSlash-1] == '/' {
+		lastSlash--
+	}
+	return link[0:lastSlash]
+}
+
+// TODO: add health check endpoint
 func (s *Server) appStart(ctx context.Context, halt <-chan struct{}) error {
-	// TODO: add health check endpoint
 	e := echo.New()
 	e.GET("/link", s.GetLink)
 	e.POST("/link", s.SaveLink)
 
-	errCh := make(chan error, 1)
+	// return 503 if halt is closed
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			select {
+			case <-halt:
+				return c.NoContent(http.StatusServiceUnavailable)
+			default:
+				return next(c)
+			}
+		}
+	})
 
+	errCh := make(chan error, 1)
 	go func() {
 		defer close(errCh)
 		var port = os.Getenv("PORT")
