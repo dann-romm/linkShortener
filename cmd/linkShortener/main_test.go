20603d05dfe775f1acfbaa94dcc69f1f155a968a
dann-romm <danilka.2002.lol@gmail.com>
2022-06-08 13:18:20 +0300

    some tests added

---
+++ cmd/linkShortener/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// test transformLink function
+func TestTransformLink(t *testing.T) {
+	assert.Equal(t, transformLink("google.com"), "google.com")
+	assert.Equal(t, transformLink("google.co"), "google.co")
+	assert.Equal(t, transformLink("google.com/"), "google.com")
+	assert.Equal(t, transformLink("http://google.com"), "google.com")
+	assert.Equal(t, transformLink("https://google.com"), "google.com")
+	assert.Equal(t, transformLink("https://google.com/////"), "google.com")
+	assert.Equal(t, transformLink("https://google.co/m/////"), "google.co/m")
+	assert.Equal(t, transformLink("///https://google.co/m"), "///https://google.co/m")
+	assert.Equal(t, transformLink("/////"), "")
+	assert.Equal(t, transformLink("https://///"), "")
+	assert.Equal(t, transformLink("https:/google.com/"), "https:/google.com")
+}
--- internal/storage/entity/link_test.go
+++ internal/storage/entity/link_test.go
@@ -8,4 +8,25 @@ import (
 func TestCreateLink(t *testing.T) {
 	assert.Equal(t, CreateLink("google.com"), CreateLink("google.com"))
 	assert.NotEqual(t, CreateLink("google.com"), CreateLink("google.co"))
+	assert.Equal(t, CreateLink(""), CreateLink(""))
+}
+
+func TestNewLink(t *testing.T) {
+	_, err := NewLink("")
+	assert.Error(t, err)
+
+	link1, err := NewLink("google.com")
+	assert.NoError(t, err)
+	link2, err := NewLink("google.com")
+	assert.NoError(t, err)
+	assert.Equal(t, link1, link2)
+}
+
+func TestValidate(t *testing.T) {
+	assert.True(t, validate(0xFBDFEC0BFD7EAF9F))
+	assert.True(t, validate(0x7FE))
+	assert.False(t, validate(0xFFE))
+	assert.False(t, validate(0x3F))
+	assert.True(t, validate(0x0))
+
 }
